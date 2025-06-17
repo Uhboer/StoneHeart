28051d65376e3368f57b9d29571571d5b83c64a6
Uhboer <grishukoleg258@gmail.com>
2025-06-17 16:28:21 +0300

    walk sound

--- scenes/player.gd
+++ scenes/player.gd
@@ -8,13 +8,11 @@ var speed = 250
 func _physics_process(delta):
 	var direction = Input.get_vector("A", "D", "W", "S")
 	velocity = direction * speed
+	if velocity && !walkSound.playing:
+		walkSound.play()
+	
 	if direction.x < 0:
 		sprite.flip_h = true
 	if direction.x > 0:
 		sprite.flip_h = false
 	move_and_slide()
-
-
-func _on_legs_body_entered(body):
-	if body.name == "tilemap":
-		walkSound.play()

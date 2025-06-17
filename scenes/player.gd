4c953eba61cbda609b5bc1a1b338a7575aa7076b
Uhboer <grishukoleg258@gmail.com>
2025-06-17 14:00:06 +0300

    move

---
+++ scenes/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+var speed = 400  
+
+@onready var sprite = $sprite
+
+
+func _physics_process(delta):
+	var direction = Input.get_vector("A", "D", "W", "S")
+	velocity = direction * speed
+	if direction.x < 0:
+		sprite.flip_h = true
+	if direction.x > 0:
+		sprite.flip_h = false
+	move_and_slide()

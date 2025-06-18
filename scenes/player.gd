3f3e5d77d8bc881243f9667c1cad96ce1671f583
NeverMind92 <llegionov2@gmail.com>
2025-06-18 18:49:34 +0400

    state

--- scenes/player.gd
+++ scenes/player.gd
@@ -7,7 +7,7 @@ var speed = 250
 @onready var arc = $arc
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction = Input.get_vector("A", "D", "W", "S")
 	velocity = direction * speed
 	if velocity && !walkSound.playing:

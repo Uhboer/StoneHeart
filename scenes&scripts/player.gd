44aaa7582c4b021ffac7453b1b96b8dba77d6b61
Uhboer <grishukoleg258@gmail.com>
2025-06-18 15:29:35 +0300

    weapons

--- scenes&scripts/player.gd
+++ scenes&scripts/player.gd
@@ -3,6 +3,7 @@ var speed = 250
 
 @onready var sprite = $sprite
 @onready var walkSound = $sounds/walk
+@onready var weapons = $weapons
 
 
 func _physics_process(delta):
@@ -13,6 +14,8 @@ func _physics_process(delta):
 	
 	if direction.x < 0:
 		sprite.flip_h = true
+		weapons.flip_h = true
 	if direction.x > 0:
 		sprite.flip_h = false
+		weapons.flip_h = false
 	move_and_slide()

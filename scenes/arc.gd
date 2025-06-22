ecb957ce01e6a95832f98bcde3306d4fd0f94dd5
Uhboer <grishukoleg258@gmail.com>
2025-06-22 14:05:08 +0300

    12332

--- scenes/arc.gd
+++ scenes/arc.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var attack = $attack
+@onready var collision = $range/collision
 
 
 func _process(delta):
@@ -8,3 +9,8 @@ func _process(delta):
 		look_at(get_global_mouse_position())
 	elif Global.attacking == true:
 		look_at(get_global_mouse_position())
+
+
+func _on_range_body_entered(body):
+	if body.name == "basemob":
+		print("collide")

2bae4251871900628785599f0181635f66b6f418
Uhboer <grishukoleg258@gmail.com>
2025-06-25 16:58:42 +0300

    works better

--- scenes/arc.gd
+++ scenes/arc.gd
@@ -8,10 +8,13 @@ var attacking = false
 func _process(delta):
 	if attacking == false:
 		look_at(get_global_mouse_position())
+		collision.disabled = true
+	if attacking:
+		collision.disabled = false
 
 
 
 
 func _on_range_body_entered(body):
-	if body.name == "basemob" && attacking:
+	if body.name == "basemob":
 		print("collide")
